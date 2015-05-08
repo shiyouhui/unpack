d8b810bb1e4a3a807d8186286bab75d34a691d97
ShiYouHui <shiyouhui@elinktek.com>
2015-05-08 17:50:42 +0800

    v2.0

--- decode.sh
+++ decode.sh
@@ -1,57 +1,135 @@
 #!/bin/bash
+USER=`whoami`
+echo $USER
+if [ $USER != "root" ];then
+	echo Example: sudo ./decode.sh
+	exit
+fi
+
 ROOT=$PWD
 SIMG2IMG=$ROOT/tool/simg2img
 UNPACK=$ROOT/tool/unpack.pl
 
-if [ -e "$ROOT"/out/userdata.img.ext4 ]; then  
-	sudo umount $ROOT/out/data/
-	rm -rf $ROOT/out/data/
-	rm "$ROOT"/out/userdata.img.ext4
-	mkdir $ROOT/out/data/
+calc()
+{
+	if [ "$1" = "r" ];then
+		return 4
+	elif [ "$1" = "w" ];then
+		return 2
+	elif [ "$1" = "x" ];then
+		return 1
+	else
+		return 0
+	fi
+}
+
+spare_mod()
+{
+	ONE=${1:0:1}
+	TWO=${1:1:1}
+	THREE=${1:2:1}
+	calc $ONE
+	RET1=$?
+	calc $TWO
+	RET2=$?
+	calc $THREE
+	RET3=$?
+	RESULT=$((RET1 + RET2 + RET3))
+	return $RESULT
+}
+
+Parser()
+{
+	REALMOD=${1:1:9}
+	ROOT_MOD=${REALMOD:0:3}
+	GROUP_MOD=${REALMOD:3:3}
+	OTHER_MOD=${REALMOD:6:3}
+	spare_mod $ROOT_MOD
+	ROOT=$?
+	spare_mod $GROUP_MOD
+	GROUP=$?
+	spare_mod $OTHER_MOD
+	OTHER=$?
+	echo $ROOT$GROUP$OTHER
+}
+
+list()
+{
+    for file in `ls $1`
+    do
+		if [ -f $file ];then
+			MOD=` ls -l $1"/"$file | awk -F" " '{print $1}'`
+		else
+			MOD=` ls -ld $1"/"$file | awk -F" " '{print $1}'`
+		fi
+		NUMMOD=`Parser "$MOD"`
+		echo $1"/"$file $NUMMOD>> $ROOT/list.txt
+		
+		if [ -d $1"/"$file ] 
+        then
+			cd $1"/"$file
+            list $1"/"$file
+        fi
+    done
+}
+if [ -e $ROOT/list.txt ];then
+	rm  $ROOT/list.txt
 fi
 
-	
-if [ -e "$ROOT"/out/system.img.ext4 ]; then 
-	sudo umount $ROOT/out/system/
-	rm -rf $ROOT/out/system/
-	rm "$ROOT"/out/system.img.ext4
-	mkdir $ROOT/out/system/
+DATE_MOUNTED=`mount | grep "/out/data"`
+if [ ! -z "$DATE_MOUNTED" ];then
+	umount $ROOT/out/data/
+fi
+
+SYSTEM_MOUNTED=`mount | grep "/out/system"`
+if [ ! -z "$SYSTEM_MOUNTED" ];then
+	umount $ROOT/out/system/
+fi
+
+if [ -e "$ROOT"/out/userdata.img.ext4 ];then
+	rm "$ROOT"/out/userdata.img.ext4
+fi
+
+if [ -e "$ROOT"/out/userdata.img.ext4 ];then
+	rm "$ROOT"/out/userdata.img.ext4
 fi
 
 rm out/* -rf
 
 if [ -e  $ROOT/img/boot.img ];then
-	mkdir out/boot
+	mkdir $ROOT/out/boot
 	$UNPACK $ROOT/img/boot.img $ROOT/out
-	echo "unpack boot.img >>>>>> OK "
+	echo "unpack boot.img >>>>>> OK"
 else
 	echo ">>>>>>Skip unpack boot.img"
 fi
 
 if [ -e  $ROOT/img/logo.bin ];then
-	mkdir out/logo
+	mkdir $ROOT/out/logo
 	$UNPACK $ROOT/img/logo.bin $ROOT/out
-	echo "unpack logo.bin >>>>>> OK "
+	echo "unpack logo.bin >>>>>> OK"
 else
 	echo ">>>>>>Skip unpack logo.bin"
 fi
 
 if [ -e  img/userdata.img ];then
-	mkdir out/data
+	mkdir  $ROOT/out/data
 	$SIMG2IMG img/userdata.img out/userdata.img.ext4
-	sudo mount -t ext4 -o loop out/userdata.img.ext4 out/data/
-	sudo chmod 777 out/data/ -R
-	echo "unpack userdata.img >>>>>> OK "
+	mount -t ext4 -o loop out/userdata.img.ext4 out/data/
+	list $ROOT/out/data
+	chmod 777 $ROOT/out/data/ -R
+	echo "unpack userdata.img >>>>>> OK"
 else
 	echo ">>>>>>Skip unpack userdata.img"
 fi
 
 if [ -e  img/system.img ];then
-	mkdir out/system
+	mkdir $ROOT/out/system
 	$SIMG2IMG img/system.img out/system.img.ext4
-	sudo mount -t ext4 -o loop out/system.img.ext4 out/system/
-	sudo chmod 777 out/system/ -R
-	echo "unpack system.img >>>>>> OK "
+	mount -t ext4 -o loop out/system.img.ext4 out/system/
+	list $ROOT/out/system
+	chmod 777 $ROOT/out/system/ -R
+	echo "unpack system.img >>>>>> OK"
 else
 	echo ">>>>>>Skip unpack system.img"
 fi	
--- encode.sh
+++ encode.sh
@@ -1,35 +1,82 @@
 #!/bin/bash
+USER=`whoami`
+echo $USER
+if [ $USER != "root" ];then
+	echo Example: sudo ./decode.sh
+	exit
+fi
+
 ROOT=$PWD
 BMP2RAW=$ROOT/tool/bmp_to_raw
 MAKE_EXT4FS=$ROOT/tool/make_ext4fs
 SIMG2IMG=$ROOT/tool/simg2img
 REPACK=$ROOT/tool/repack.pl
 MKBOOTIMG=$ROOT/tool/mkbootimg
-FILE_CONTEXTS=$ROOT/out/boot/boot-ramdisk/file_contexts
+if [ -e $ROOT/out/system.img.ext4 ];then
+	PLATFORM=`awk -F"=" '{if(/^ro.mediatek.platform/)print $2}' $ROOT/out/system/build.prop`
+	RELEASE=`awk -F"=" '{if(/^ro.build.version.release/)print $2}' $ROOT/out/system/build.prop`
+	OLD_RELEASE=(4.2.2 4.4.2 4.1.2 4.4.4)
+	if [ "$PLATFORM" = "mt6582" ];then
+		if [ "RELEASE" = "5.0" ];then
+			FILE_CONTEXTS=$ROOT/root/lo82/file_contexts
+		fi
+	elif [  "$PLATFORM" = "mt6752" ];then
+		if [ "RELEASE" = "5.0" ];then
+			FILE_CONTEXTS=$ROOT/root/lo52/file_contexts
+		fi
+	elif [  "$PLATFORM" = "mt8127" ];then
+		if [ "RELEASE" = "5.0" ];then
+			FILE_CONTEXTS=$ROOT/root/lo27/file_contexts
+		fi
+	elif [  "$PLATFORM" = "mt6735" ];then
+		if [ "RELEASE" = "5.0" ];then
+			FILE_CONTEXTS=$ROOT/root/lo35/file_contexts
+		elif [ "RELEASE" = "5.1" ];then
+			FILE_CONTEXTS=$ROOT/root/lo35m/file_contexts
+		fi
+	fi
+fi
+
+set_mod()
+{
+	for file in `awk -F" " '{print $1}' list.txt`
+	do
+		 MOD=`grep "$file " list.txt | awk -F" " '{print $2}'`
+		 chmod $MOD $file
+	done
+}
+
+set_mod
 
 if [ -e $ROOT/out/system.img.ext4 ];then
 	SYSTEMSIZE=`ls -alh $ROOT/out/system.img.ext4 | awk '{print $5}'`
-	echo "system.img size is $SYSTEMSIZE"
 	SYSTEMSIZE_1=`ls -al $ROOT/out/system.img.ext4 | awk '{print $5}'`
-	$MAKE_EXT4FS -s -T -1 -S $FILE_CONTEXTS -l $SYSTEMSIZE_1 -a system $ROOT/out/system.img $ROOT/out/system/
-	sudo fuser -km $ROOT/out/system/
-	sudo umount $ROOT/out/system/
-	rm $ROOT/out/system.img.ext4 -f 
+	echo ${OLD_RELEASE[@]} | grep -wq "$RELEASE"
+	if [ $? = 0 ];then
+		$MAKE_EXT4FS -s -T -1 -l $SYSTEMSIZE_1 -a system $ROOT/out/system.img $ROOT/out/system/
+	else
+		$MAKE_EXT4FS -s -T -1 -S $FILE_CONTEXTS -l $SYSTEMSIZE_1 -a system $ROOT/out/system.img $ROOT/out/system/
+	fi
+	fuser -km $ROOT/out/system/
+	umount $ROOT/out/system/
+	rm $ROOT/out/system.img.ext4 -f
 fi
 
 if [ -e $ROOT/out/userdata.img.ext4 ];then
 	USERDATASIZE=`ls -alh $ROOT/out/userdata.img.ext4 | awk '{print $5}'`
-	echo "userdata.img size is $USERDATASIZE"
 	USERDATASIZE_1=`ls -al $ROOT/out/userdata.img.ext4 | awk '{print $5}'`
-	$MAKE_EXT4FS -s -T -1 -S $FILE_CONTEXTS -l $USERDATASIZE_1 -a system $ROOT/out/userdata.img $ROOT/out/data/
-	sudo fuser -km $ROOT/out/data/
-	sudo umount $ROOT/out/data/
+	if [ $? = 0 ];then
+		$MAKE_EXT4FS -s -T -1 -l $USERDATASIZE_1 -a system $ROOT/out/userdata.img $ROOT/out/data/
+	else
+		$MAKE_EXT4FS -s -T -1 -S $FILE_CONTEXTS -l $USERDATASIZE_1 -a system $ROOT/out/userdata.img $ROOT/out/data/
+	fi
+	fuser -km $ROOT/out/data/
+	umount $ROOT/out/data/
 	rm $ROOT/out/userdata.img.ext4 -f 
 fi
 
 if [ -e $ROOT/out/boot ];then
 	$REPACK -boot $ROOT/out/boot/boot-kernel.img $ROOT/out/boot/boot-ramdisk $ROOT/out/boot.img
-	rm $ROOT/out/boot -rf
 fi
 
 if [ -e $ROOT/out/logo ];then
